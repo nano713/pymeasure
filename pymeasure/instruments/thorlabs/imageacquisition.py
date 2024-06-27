5d34180da7334d611bb7642ccb8162f290269e9f
adeshazer123 <adeshazer@wisc.edu>
2024-06-27 10:57:11 +0900

    add def __exit__ and def __enter__ statements to uphold with functionality

--- pymeasure/instruments/thorlabs/imageacquisition.py
+++ pymeasure/instruments/thorlabs/imageacquisition.py
@@ -14,7 +14,10 @@ except ImportError:
 
 import os
 import queue
+import logging
 from ctypes import c_longlong
+
+from exceptiongroup import format_exception
 from pymeasure.instruments import Instrument
 from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCameraError, _create_c_failure_message
 from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
@@ -22,14 +25,25 @@ from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
 from PIL import Image
 import numpy as np 
 # import tkinter as tk
+_logger = logging.getLogger('thorlabs_tsi_sdk.tl_camera')
 
-class CS165MUM(Instrument):
+class CS165MUM():
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, camera_connection = None):
+        self.camera = camera_connection
+        print("Camera connected")
     
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        if exception_type is not None:
+            _logger.debug("".join(format_exception(exception_type, exception_value, exception_traceback)))
+        self.dispose()
+        return True if exception_type is None else False
+
     def set_parameters(self):
-        with Image_Parameters() as image_processes:
+        with Image_Parameters(self.name) as image_processes:
             image_processes.get_exposure_time_us()
             image_processes.set_exposure_time_us()
             print("Parameters set")
@@ -38,7 +52,9 @@ class CS165MUM(Instrument):
             image_save.save_image(image_acquisition.last_image, "saved_image.tif")
         print("Image saved")
 
-class Image_Parameters(): 
+class Image_Parameters():
+    def __init__(self, camera):
+        self._camera = camera 
         @property
         def get_exposure_time_us(self): 
             try: 
@@ -72,106 +88,106 @@ class Image_Parameters():
             image.save(file_path, format = 'TIF')
             print(f"Image saved to {file_path}")
 
-class ImageAcquisitionThread():
+# class ImageAcquisitionThread():
 
-    def __init__(self, camera):
-        # type: (TLCamera) -> ImageAcquisitionThread
-        super(ImageAcquisitionThread, self).__init__()
-        self._camera = camera
-        self._previous_timestamp = 0 
+#     def __init__(self, camera):
+#         # type: (TLCamera) -> ImageAcquisitionThread
+#         super(ImageAcquisitionThread, self).__init__()
+#         self._camera = camera
+#         self._previous_timestamp = 0 
         
-        #Checks if whether can incorporate color processing
-        if self._camera.camera_get_sensor != SENSOR_TYPE.BAYER:
-            # Cannot support color
-            self._camera = False
-        else:
-            self._mono_to_color_sdk = MonoToColorProcessorSDK()
-            self._image_width = self._camera.image_width_pixels
-            self._image_height = self._camera.image_height_pixels
-            self._mono_to_color_processor = self._mono_to_color_sdk.create_mono_to_color_processor(
-                SENSOR_TYPE.BAYER,
-                self._camera.color_filter_array_phase,
-                self._camera.get_color_correction_matrix(),
-                self._camera.get_default_white_balance_matrix(),
-                self._camera.bit_depth
-            )
-            self._is_color = True
+#         #Checks if whether can incorporate color processing
+#         if self._camera.camera_get_sensor != SENSOR_TYPE.BAYER:
+#             # Cannot support color
+#             self._camera = False
+#         else:
+#             self._mono_to_color_sdk = MonoToColorProcessorSDK()
+#             self._image_width = self._camera.image_width_pixels
+#             self._image_height = self._camera.image_height_pixels
+#             self._mono_to_color_processor = self._mono_to_color_sdk.create_mono_to_color_processor(
+#                 SENSOR_TYPE.BAYER,
+#                 self._camera.color_filter_array_phase,
+#                 self._camera.get_color_correction_matrix(),
+#                 self._camera.get_default_white_balance_matrix(),
+#                 self._camera.bit_depth
+#             )
+#             self._is_color = True
             
-        self._bit_depth = camera.bit_depth
-        self._camera.image_poll_timeout_ms = 0  # Do not want to block for long periods of time
-        # self._camera.exposure_time_us = 1000 # initializes the exposure time in seconds
-        # self._image_queue = queue.Queue(maxsize=2)
+#         self._bit_depth = camera.bit_depth
+#         self._camera.image_poll_timeout_ms = 0  # Do not want to block for long periods of time
+#         # self._camera.exposure_time_us = 1000 # initializes the exposure time in seconds
+#         # self._image_queue = queue.Queue(maxsize=2)
 
-    def get_output_queue(self): 
-        return self._image_queue
+#     def get_output_queue(self): 
+#         return self._image_queue
 
-    def _get_color_image(self, frame):
-        # type: (Frame) -> Image
-        # verify the image size
-        width = frame.image_buffer.shape[1]
-        height = frame.image_buffer.shape[0]
-        if (width != self._image_width) or (height != self._image_height):
-            self._image_width = width
-            self._image_height = height
-            print("Image dimension change detected, image acquisition thread was updated")
-        # color the image. transform_to_48 will scale to 16 bits per channel
-        color_image_data = self._mono_to_color_processor.transform_to_48(frame.image_buffer,
-                                                                         self._image_width,
-                                                                         self._image_height)
-        color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
-        print("color_image_data =", color_image_data)
-        # return PIL Image object
-        return Image.fromarray(color_image_data, mode='RGB')
+#     def _get_color_image(self, frame):
+#         # type: (Frame) -> Image
+#         # verify the image size
+#         width = frame.image_buffer.shape[1]
+#         height = frame.image_buffer.shape[0]
+#         if (width != self._image_width) or (height != self._image_height):
+#             self._image_width = width
+#             self._image_height = height
+#             print("Image dimension change detected, image acquisition thread was updated")
+#         # color the image. transform_to_48 will scale to 16 bits per channel
+#         color_image_data = self._mono_to_color_processor.transform_to_48(frame.image_buffer,
+#                                                                          self._image_width,
+#                                                                          self._image_height)
+#         color_image_data = color_image_data.reshape(self._image_height, self._image_width, 3)
+#         print("color_image_data =", color_image_data)
+#         # return PIL Image object
+#         return Image.fromarray(color_image_data, mode='RGB')
 
-    def _get_image(self, frame):
-        # type: (Frame) -> Image
-        # no coloring, just scale down image to 8 bpp and place into PIL Image object
-        scaled_image = frame.image_buffer >> (self._bit_depth - 8)
-        return Image.fromarray(scaled_image)
+#     def _get_image(self, frame):
+#         # type: (Frame) -> Image
+#         # no coloring, just scale down image to 8 bpp and place into PIL Image object
+#         scaled_image = frame.image_buffer >> (self._bit_depth - 8)
+#         return Image.fromarray(scaled_image)
     
 
-    def run(self):
-        while not self._stop_event.is_set():
-            try:
-                frame = self._camera.get_pending_frame_or_null()
-                if frame is not None:
-                    if self._is_color:
-                        pil_image = self._get_color_image(frame)
-                    else:
-                        pil_image = self._get_image(frame)
-                    self._image_queue.put_nowait(pil_image)
-            except queue.Full:
-                # No point in keeping this image around when the queue is full, let's skip to the next one
-                pass
-            except Exception as error:
-                print("Encountered error: {error}, image acquisition will stop.".format(error=error))
-                break
-        print("Image acquisition has stopped")
-        if self._is_color:
-            self._mono_to_color_processor.dispose()
-            self._mono_to_color_sdk.dispose()
+#     def run(self):
+#         while not self._stop_event.is_set():
+#             try:
+#                 frame = self._camera.get_pending_frame_or_null()
+#                 if frame is not None:
+#                     if self._is_color:
+#                         pil_image = self._get_color_image(frame)
+#                     else:
+#                         pil_image = self._get_image(frame)
+#                     self._image_queue.put_nowait(pil_image)
+#             except queue.Full:
+#                 # No point in keeping this image around when the queue is full, let's skip to the next one
+#                 pass
+#             except Exception as error:
+#                 print("Encountered error: {error}, image acquisition will stop.".format(error=error))
+#                 break
+#         print("Image acquisition has stopped")
+#         if self._is_color:
+#             self._mono_to_color_processor.dispose()
+#             self._mono_to_color_sdk.dispose()
 
 
-    def dispose(self):
-        # type: (type(None)) -> None
-        """
-        Cleans up the TLCameraSDK instance - make sure to call this when you are done with the TLCameraSDK instance.
-        If using the *with* statement, dispose is called automatically upon exit.
+#     def dispose(self):
+#         # type: (type(None)) -> None
+#         """
+#         Cleans up the TLCameraSDK instance - make sure to call this when you are done with the TLCameraSDK instance.
+#         If using the *with* statement, dispose is called automatically upon exit.
 
-        """
-        try:
-            if self._disposed:
-                return
-            error_code = self._sdk.tl_camera_close_sdk()
-            if error_code != 0:
-                raise TLCameraError(_create_c_failure_message(self._sdk, "tl_camera_close_sdk", error_code))
-            TLCameraSDK._is_sdk_open = False
-            self._disposed = True
-            self._current_camera_connect_callback = None
-            self._current_camera_disconnect_callback = None
-        except Exception as exception:
-            print("Camera SDK destruction failed; " + str(exception))
-            raise exception
+#         """
+#         try:
+#             if self._disposed:
+#                 return
+#             error_code = self._sdk.tl_camera_close_sdk()
+#             if error_code != 0:
+#                 raise TLCameraError(_create_c_failure_message(self._sdk, "tl_camera_close_sdk", error_code))
+#             TLCameraSDK._is_sdk_open = False
+#             self._disposed = True
+#             self._current_camera_connect_callback = None
+#             self._current_camera_disconnect_callback = None
+#         except Exception as exception:
+#             print("Camera SDK destruction failed; " + str(exception))
+#             raise exception
 
 
 if "__main__" == __name__:
@@ -182,24 +198,24 @@ if "__main__" == __name__:
         else: 
             print("No camera detected")
     
-        with sdk.open_camera(camera_list[0]) as camera:
-            camera.frames_per_trigger_zero_for_unlimited = 0  # start camera in continuous mode
-            camera.arm(2)
-            camera.issue_software_trigger()
+        with sdk.open_camera(camera_list[0]) as camera_connection:
+            camera_connection.frames_per_trigger_zero_for_unlimited = 0  # start camera in continuous mode
+            camera_connection.arm(2)
+            camera_connection.issue_software_trigger()
 
-        with CS165MUM("27886") as set_exposure: 
+        with CS165MUM(camera_connection) as set_exposure: 
             print(set_exposure.name)
             exposure = set_exposure.set_parameters()
-            camera.issue_software_trigger()
+            camera_connection.issue_software_trigger()
 
-        with ImageAcquisitionThread(camera) as image_acquisition: 
-            print("Image acquisition started")
-            print("Image acquisition finished")
+        # with ImageAcquisitionThread(camera_connection) as image_acquisition: 
+        #     print("Image acquisition started")
+        #     print("Image acquisition finished")
 
-        with Image_Parameters(camera) as image_save:
-            image_save.save_image(image_acquisition.last_image, "saved_image.tif")
+        # with Image_Parameters(camera_connection) as image_save:
+        #     image_save.save_image(image_acquisition.last_image, "saved_image.tif")
         
-        camera.disarm() 
+        camera_connection.disarm() 
         print("Camera disarmed")
 
 
