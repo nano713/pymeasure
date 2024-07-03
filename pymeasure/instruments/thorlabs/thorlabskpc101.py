321389e4caf427d11127ee6d1715cb6d00c86a13
adeshazer123 <adeshazer@wisc.edu>
2024-07-03 13:16:29 +0900

    create KPC101 python file and log handle

---
+++ pymeasure/instruments/thorlabs/thorlabskpc101.py
@@ -0,0 +1,9 @@
+# Author: Amelie Deshazer 
+# Date: 2024-07-03
+# Purpose: This script will control the piezoelectric stage to move the reference arm. 
+
+import logging 
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
