a02b832c50de8c2b52b296de4e7433a797d4cf40
adeshazer123 <adeshazer@wisc.edu>
2024-07-09 13:09:20 +0900

    delete print statements. Fix debug errors

--- pymeasure/instruments/thorlabs/thorlabskpz101.py
+++ pymeasure/instruments/thorlabs/thorlabskpz101.py
@@ -8,6 +8,7 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
 import clr 
+import System
 
 clr.AddReference(r"C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.DeviceManagerCLI")
 clr.AddReference(r"C:\Program Files\Thorlabs\Kinesis\Thorlabs.MotionControl.GenericMotorCLI.dll")
@@ -21,62 +22,57 @@ from Thorlabs.MotionControl.KCube.PiezoCLI import *
 from System import Decimal
 import time
 
-print("reading the class...")
+
 class KPZ101():
     def __init__(self):
         DeviceManagerCLI.BuildDeviceList()
-        serial_number = '' #must add serial number
+        serial_number = '29252556' #must add serial number
         self.device = KCubePiezo.CreateKCubePiezo(serial_number)
 
         self.device.Connect(serial_number)
 
         info_device = self.device.GetDeviceInfo()
-        print(info_device.Description)
+        log.info(info_device.Description)
 
 
         self.device.StartPolling(250)
         time.sleep(0.25)
         log.info("Device polling started")
-        print("Device polling started") #for debugging purposes
 
         self.device.EnableDevice()
         time.sleep(0.25)
-        log.info("Device enabled")
-        print("Device enabled") #for debugging purposes 
+        log.info("Device enabled") 
 
-        device_config = self.device.GetPiezoConfiguration()
-        print("Device configuration is %s" % device_config) #for debugging purposes
-        log.print("Device configuration is %s" % device_config)
+        device_config = self.device.GetPiezoConfiguration(serial_number)
+        log.info("Device configuration is %s" % device_config)
 
-        device_settings = self.device.PiezoDeviceSettings()
+        device_settings = self.device.PiezoDeviceSettings
         
     def move_home(self):
         self.device.SetZero() 
         log.info("Device moved to home position")
-        print("Device moved to home position") #for debugging purposes
     
     def max_voltage(self): 
-        max_voltage = self.device.GetVoltageOutputMax()
+        max_voltage = self.device.GetMaxOutputVoltage()
         return max_voltage
     
     def set_voltage(self,voltage):
-        #self.device.SetVoltageOutput(voltage)
-        # checks bounds of voltage parameter
-        if voltage != Decimal(0) & voltage <= self.max_voltage():
-            self.device.SetVoltageOutput(voltage)
+        voltage = Decimal(voltage)
+    
+        min_volt = System.Decimal(0)
+        max_volt = self.max_voltage()
+        if voltage != min_volt and voltage <= max_volt:
+            self.device.SetOutputVoltage(voltage)
             time.sleep(1.0)
             log.info("Voltage set to %s" % voltage)
-            print("Voltage set to %s" % voltage) #for debugging purposes
 
         else: 
-            print(f"Voltage must be between 0 and {self.max_voltage()}")
-            log.error(f"Voltage must be between 0 and {self.max_voltage()}")
+            log.error(f"Voltage must be between 0 and {max_volt}")
     
     def disconnect(self): 
         self.device.StopPolling()
-        self.device.Disconnet()
+        self.device.Disconnect()
         log.info("Device has been disconnected")
-        print("Device has been disconnected") #for debugging purposes
 
 
 
